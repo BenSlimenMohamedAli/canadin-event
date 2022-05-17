c9755799e8db6d5fcc2e24a46193c280da024994
dali15 <dslimen.15@gmail.com>
2022-05-17 10:05:22 +0100

    add the new videos

--- components/AnnualModel.vue
+++ components/AnnualModel.vue
@@ -21,14 +21,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <iframe
-              id="ytplayer"
-              class="embed-responsive-item"
-              src="https://www.youtube.com/embed/wFfRI5XcewY"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
+           <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/710381489?h=601d9f2a64&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="master class throwback"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
           </div>
         </div>
       </div>
